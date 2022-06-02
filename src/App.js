6656bb38faeacdaf2c803aacc98339881795d267
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-02 22:46:33 +0900

    Upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { useState } from 'react';
 function App() {
   console.log('render');
   const [number, setNumber] = useState(1);
-  const [number1, setNumber1] = useState(1);
   const double = () => {
-    const doubleNumber = number * 3;
-    setNumber(doubleNumber);
-    setNumber1(number1 *3);
-    console.log(doubleNumber);
+    // const doubleNumber = number * 2;
+    setNumber((prevState) => {
+      return prevState *2
+    });
+    setNumber((prevState) => {
+      return prevState *2
+    });
   };
 
   return (
     <>
       <div>{number}</div>
-      <div>{number1}</div>
       <button onClick={double}>Sumbit</button>
     </>
   );
