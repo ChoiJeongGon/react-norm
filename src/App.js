17c42bf26eb933f381d130b4c58bda7ba41fcd8d
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-02 22:47:52 +0900

    Upload

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   const [number, setNumber] = useState(1);
   const double = () => {
     // const doubleNumber = number * 2;
-    setNumber((prevState) => {
-      return prevState *2
-    });
+    setNumber((prevState) => prevState *2);
+    
     setNumber((prevState) => {
       return prevState *2
     });
