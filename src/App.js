74b74334ea28c1f89d1f53787b432659423e53f4
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-09 17:26:55 +0900

    Upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { BrowserRouter as 
-  Router, 
-  Switch, 
-  Route, 
-  Link 
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [title, setTitle] = useState("");
