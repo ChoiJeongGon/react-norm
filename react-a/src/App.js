c984da8009c1811ff94ef0d457795add15da416f
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-16 22:55:32 +0900

    Upload 1

--- react-a/src/App.js
+++ react-a/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [time, setTime] = useState(1);
+
+ const handleClick = () => {
+  let newTime;
+  if (time >= 12){
+    newTime = 1;
+  }else{
+    newTime = time + 1;
+  }
+  setTime(newTime);
+ };
+ console.log('업데이트');
+
+ return(
+  <div>
+    <span>현재 시각:{time}시각</span>
+    <button onClick={handleClick}>Update</button>
+  </div>
+ )
+
 }
 
 export default App;
