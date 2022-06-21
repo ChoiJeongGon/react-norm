0135a14f0c0204af20464c4c85e6a547cee2e57f
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-21 18:20:29 +0900

    Upload 1

--- react-a/src/App.js
+++ react-a/src/App.js
@@ -1,29 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [names, setNames] = useState(()=>{
-    return heavyWork();
-  });
-  const [input, setInput] = useState("");
 
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-  const handleleUpload = () => {
-    setNames((preState) => {
+  const [count, setCount] = useState(1);
 
-      return [input, ...preState];
-    });
+  const handleCountUpdate = () => {
+
   };
-  return (
+
+  return(
     <div>
-      <input type="text" value={input} onChange={handleInputChange} />
-      <button onClick={handleleUpload}>Upload</button>
-      {names.map((name, idx) => {
-        return <p key={idx}>{name}</p>;
-      })}
+      <button onClick={handleCountUpdate}>Updata</button>
+      <span>count : {count} </span>
     </div>
-  );
+  )
+
 }
 
 export default App;
