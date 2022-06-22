891fdf3247d5628acc23d7f38efe69f51823d6a9
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-22 22:22:26 +0900

    Upload 1

--- react-a/src/App.js
+++ react-a/src/App.js
@@ -1,36 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Timer from "./component/Timer";
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [name, setName] = useState("");
-
-  const handleCountUpdate = () => {
-    setCount(count + 1);
-  };
-
-  const handleInputChange = (e) => {
-    setName(e.target.value);
-  };
-
-  // 렌더링마다 매번 실행됨 -렌더링 이후
-  useEffect(() => {
-    console.log("렌더링");
-  });
-  // 마운트 + count 변경될때만 실행
-  useEffect(() => {
-    console.log(" count 렌더링");
-  }, [count]);
-  // 마운트 + name 변경될때만 실행
-  useEffect(() => {
-    console.log(" name 렌더링");
-  }, [name]);
-
+  const [showTimer, setShowTimer] = useState(false);
   return (
     <div>
-      <button onClick={handleCountUpdate}>Updata</button>
-      <span>count : {count} </span>
-      <input type="text" value={name} onChange={handleInputChange} />
-      <span>name : {name}</span>
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
     </div>
   );
 }
