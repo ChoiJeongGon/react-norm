cdf603aa942c251216a58ee0005535acd10e617e
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-23 23:16:03 +0900

    Upload 1

--- react-a/src/App.js
+++ react-a/src/App.js
@@ -1,18 +1,27 @@
-import React, { useState} from "react";
+import React, { useState, useRef} from "react";
 
-function App() {
+const App = () => {
     const [count, setCount] = useState(0);
+    const countRef = useRef(0);
+
+    console.log('렌더링');
 
     const increaseCountState = () => {
       setCount(count + 1);
+    };
+    const increaseCountRef = () => {
+      countRef.current = countRef.current + 1;
+      console.log("Ref :", countRef.current);
     };
     
     return(
       <div>
         <p>State: {count}</p>
+        <p>Ref: {countRef.current}</p>
         <button onClick={increaseCountState}>State UP</button>
+        <button onClick={increaseCountRef}>Ref UP</button>
       </div>
-    )
+    );
   };
 
 export default App;
