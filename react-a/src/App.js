efb19c4c638f32f94001b8596203e352660d6ef0
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-23 23:29:31 +0900

    Upload 1

--- react-a/src/App.js
+++ react-a/src/App.js
@@ -1,27 +1,21 @@
-import React, { useState, useRef} from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const App = () => {
-    const [count, setCount] = useState(0);
-    const countRef = useRef(0);
+  const [count, setCount] = useState(1);
+  const rederCount = useRef(1);
 
-    console.log('렌더링');
+  useEffect(() => {
+    rederCount.current = rederCount.current + 1;
+    console.log("렌더링 :", rederCount.current);
+  });
 
-    const increaseCountState = () => {
-      setCount(count + 1);
-    };
-    const increaseCountRef = () => {
-      countRef.current = countRef.current + 1;
-      console.log("Ref :", countRef.current);
-    };
-    
-    return(
-      <div>
-        <p>State: {count}</p>
-        <p>Ref: {countRef.current}</p>
-        <button onClick={increaseCountState}>State UP</button>
-        <button onClick={increaseCountRef}>Ref UP</button>
-      </div>
-    );
-  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Up</button>
+    </div>
+  );
+};
 
 export default App;
