a499d85c7fa322dd4c1dd8b5897721533a9bae23
Choi Jeong Gon <shinbaji@gmail.com>
2022-06-27 23:27:03 +0900

    Upload 1

--- react-a/src/App.js
+++ react-a/src/App.js
@@ -1,11 +1,21 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 
 const App = () =>{
   const inputRef = useRef()
+
+  useEffect(() =>{
+   // console.log(inputRef);
+   inputRef.current.focus();
+  }, []);
+
+  const login = () => {
+    alert(`환영합니다 ${inputRef.current.value}!`);
+    inputRef.current.focus();
+  }
   return(
     <div>
-      <input type="text" placeholder="username"></input>
-      <button>Login</button>
+      <input ref={inputRef} type="text" placeholder="username"></input>
+      <button onClick={login}>Login</button>
     </div>
   )
 
